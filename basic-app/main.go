9a8d578ec6a653e9d7a2a2ccb311be1be0b04de8
twill <twill1@gmx.com>
2023-04-06 22:52:30 -0500

    3.3

--- basic-app/main.go
+++ basic-app/main.go
@@ -5,14 +5,26 @@ import (
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		n, err := fmt.Fprintf(res, "Hello World")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Printf("Bytes written:%d\n", n)
-	})
+const portNumber = ":8008"
 
-	_ = http.ListenAndServe(":8008", nil)
+func Home(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "This is the homepage")
+}
+
+func About(res http.ResponseWriter, req *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(res, fmt.Sprintf("This is the about page 2 + 2 is %d", sum))
+}
+
+func addValues(x, y int) int {
+	sum := x + y
+	return sum
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Printf("Starting Application on port %s", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
 }
