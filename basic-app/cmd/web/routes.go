31763848d7b13ee69fd28f7f47d7216f7302abd2
twill <twill1@gmx.com>
2023-04-09 12:00:48 -0500

    4.2

--- basic-app/cmd/web/routes.go
+++ basic-app/cmd/web/routes.go
@@ -3,16 +3,20 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/isoment/basic-app/pkg/config"
 	"github.com/isoment/basic-app/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	mux := chi.NewRouter()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// We can use the built in chi middleware like so
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
