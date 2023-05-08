d2840c2fcc48777944b868a516068dec2af50fff
twill <twill1@gmx.com>
2023-05-08 00:02:09 -0500

    13.8

--- booking-app/internal/repository/dbrepo/postgres.go
+++ booking-app/internal/repository/dbrepo/postgres.go
@@ -21,7 +21,7 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 	stmt := `insert into reservations (
 				first_name, last_name, email, phone, start_date, end_date,
 				room_id, created_at, updated_at
-			) values ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+			) values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning id`
 
 	// We can pass in the context when executing the query
 	_, err := m.DB.ExecContext(ctx, stmt,
