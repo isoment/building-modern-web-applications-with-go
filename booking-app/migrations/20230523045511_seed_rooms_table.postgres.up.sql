11647cf9dfa06fc584e4566f9c55b7087774857b
twill <twill1@gmx.com>
2023-05-22 23:59:45 -0500

    13.21

---
+++ booking-app/migrations/20230523045511_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General''s Quarters','2023-05-07 23:12:13.604','2023-05-07 23:12:16.210'),
+	 ('Major''s Suite','2023-05-10 00:20:29.487','2023-05-10 00:20:32.577');
---
+++ booking-app/migrations/20230523045807_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Reservation','2023-05-09 00:15:36.046','2023-05-09 00:15:38.686'),
+	 ('Owner Block','2023-05-10 00:10:18.855','2023-05-10 00:10:21.540');
